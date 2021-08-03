53d279be8681230e765d164b828fe88db579642f
Viktoriia <vika.bila97@gmail.com>
2021-08-03 16:29:06 +0300

    init: fix mistakes
    add missed curly brackets
    connect path-module
    install webpack-merge

--- react-components/webpack.dev.js
+++ react-components/webpack.dev.js
@@ -1,6 +1,13 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
   devtool: 'inline-source-map',
+  devServer: {
+    open: true,
+    hot: true,
+    port: 8080,
+    contentBase: path.join(__dirname, 'public')
+  }
 });
