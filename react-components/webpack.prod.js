69e2bf2cd755460861fd55722410c147a506e9dc
Viktoriia <vika.bila97@gmail.com>
2021-08-04 10:07:50 +0300

    init: configure eslint

--- react-components/webpack.prod.js
+++ react-components/webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const TerserPlugin = require('terser-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -8,4 +9,7 @@ module.exports = merge(common, {
   optimization: {
     minimizer: [new TerserPlugin()],
   },
+  plugins: [
+    new ESLintPlugin({ extensions: ['js', 'ts'] })
+  ]
 });
