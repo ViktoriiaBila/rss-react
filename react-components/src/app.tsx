a91dbec669b7c0fef304d0589c328eb94c9cf248
Viktoriia <vika.bila97@gmail.com>
2021-08-16 09:25:21 +0300

    feat: add data

--- react-components/src/app.tsx
+++ react-components/src/app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles/styles.scss';
 import { SearchBar } from './components/searchBar';
 import { CardsField } from './components/cardsField';
+import { cards } from './constants/cards';
 
 export function App(): JSX.Element {
   return (
