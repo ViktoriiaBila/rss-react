234cf0b316dda61f31e35d7451109596033328e2
Viktoriia <vika.bila97@gmail.com>
2021-09-02 13:30:18 +0300

    feat: add styles to photo field and photo elements

--- react-api/src/components/photosField/index.tsx
+++ react-api/src/components/photosField/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { PhotoElement } from './photoElement';
+import './photoField.scss';
 
 const sizeSuffix = 'q';
 
 export function PhotosField(props: IPhotosFieldProps): JSX.Element {
   return (
-    <div>
+    <div className="photoField">
       {props.photos.map((photo, index: number) => (
         <PhotoElement
           key={index}
--- react-api/src/components/photosField/photoElement/index.tsx
+++ react-api/src/components/photosField/photoElement/index.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 
 export function PhotoElement(props: IPhotoElementProps): JSX.Element {
   return (
-    <div>
-      <img src={props.url} alt={props.alt} />
-      <p>{props.owner}</p>
-      <p>{props.title}</p>
+    <div className="photoField__photoElement">
+      <div className="photoField__photoElement__photo__container">
+        <img
+          className="photoField__photoElement__photo"
+          src={props.url}
+          alt={props.alt}
+        />
+      </div>
+      <div className="photoField__photoElement__text">
+        <p className="photoField__photoElement__text__owner">{props.owner}</p>
+        <div className="photoField__photoElement__text__hr"></div>
+        <p className="photoField__photoElement__text__title">{props.title}</p>
+      </div>
     </div>
   );
 }
